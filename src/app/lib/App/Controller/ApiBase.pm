c073d053860cb435fef99af9e88b498d49dabc4b
Takuho Yoshizu <yoshizu@s2factory.co.jp>
2013-05-17 14:59:08 +0900

    エラープラグイン読み込みのテンプレートを追記

--- src/app/lib/App/Controller/ApiBase.pm
+++ src/app/lib/App/Controller/ApiBase.pm
@@ -14,6 +14,14 @@ sub will_dispatch {
 #			SuppressEmpty => '',
 #			XMLDecl       => '<?xml version="1.0" encoding="utf-8"?>'
 #		},
+#		'Error' => {
+#			LACK_OF_PARAM   => sub { shift->throw(1001, sprintf("missing mandatory parameters: %s", $_[0] || "")) },
+#			INVALID_PARAM   => sub { shift->throw(1002, sprintf("illegal parameter: %s", $_[0] || "")) },
+#			NEED_TO_LOGIN   => sub { shift->throw(1003, sprintf("You must login.")) },
+#			SNS_LIMIT_ERROR => sub { shift->throw(2001, sprintf("SNS Limit Error: reset at %s", $_[0] || "")) },
+#			SNS_ERROR       => sub { shift->throw(2002, sprintf("SNS Error: %s", $_[0] || "SNS Error.")) },
+#			ERROR           => sub { shift->throw(400,  sprintf("%s", $_[0] || "Unknown Error.")) },
+#		},
 		'CGI::Session' => {
 			dbh           => db('master')->dbh,
 			table         => SES_TABLE,
