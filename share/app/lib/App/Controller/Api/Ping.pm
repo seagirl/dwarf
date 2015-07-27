352f5b4130037ac39970267eaf8a0b6e05d8278c
Takuho Yoshizu <yoshizu@s2factory.co.jp>
2015-07-27 17:40:45 +0900

    Ping API の出力に動作環境を出力する

--- share/app/lib/App/Controller/Api/Ping.pm
+++ share/app/lib/App/Controller/Api/Ping.pm
@@ -4,7 +4,11 @@ use parent 'App::Controller::ApiBase';
 use Dwarf::DSL;
 
 sub get {
-	return {};
+	return {
+		hostname    => c->hostname,
+		base_dir    => c->base_dir,
+		config_name => c->config_name,
+	};
 }
 
 1;
