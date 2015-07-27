b472e4c4b46642d3a50de28f89db47a9663455ae
Takuho Yoshizu <yoshizu@s2factory.co.jp>
2015-07-27 15:35:34 +0900

    個人開発環境の設定ファイルを開発環境の設定ファイルを元に作るようにしてみる

--- share/app/lib/App/Config/DevYoshizu.pm
+++ share/app/lib/App/Config/DevYoshizu.pm
@@ -1,50 +1,15 @@
 package App::Config::DevYoshizu;
 use Dwarf::Pragma;
-use parent 'Dwarf::Config';
+use parent 'App::Config::Development';
 
 sub setup {
 	my $self = shift;
 	return (
+		$self->SUPER::setup,
 		ssl => 0,
 		url => {
 			base     => 'http://<APP_NAME>.seagirl.local',
 			ssl_base => 'https://<APP_NAME>.seagirl.local',
-		},
-		db => {
-			master => {
-				dsn      => 'dbi:Pg:dbname=<APP_NAME>',
-				username => 'www',
-				password => '',
-				opts     => { pg_enable_utf8 => 1 },
-			},
-		},
-		session => {
-			store => {
-				table => 'sessions',
-			},
-			state => {
-				name  => '<APP_NAME>_sid',
-			},
-		},
-		filestore => {
-			private => {
-				dir => $self->c->base_dir . "/filestore",
-				uri => "/filestore",
-			},
-			public  => {
-				dir => $self->c->base_dir . "/../htdocs/filestore",
-				uri => "/filestore",
-			},
-		},
-		app => {
-			facebook => {
-				id     => '',
-				secret => '',
-			},
-			twitter  => {
-				id     => '',
-				secret => '',
-			}
 		},
 	);
 }
