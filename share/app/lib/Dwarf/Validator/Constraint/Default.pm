ddb6b00066b2b3e0276ee630c2e8f242692a1074
Naoko Umeta <umeta@s2factory.co.jp>
2015-09-15 19:42:52 +0900

    FILE_EXT のルールを変更
    FILE_EXT のルールを使ってバリデーションしたときに、
    ファイル名が foo.bar.baz.jpg などのように . が複数入っていると
    最初に現れる . 以降が拡張子として扱われてしまいます。
    そのため、jpg でチェックしても validation error となってしまうため、
    最後に現れる . 以降を拡張子として扱いたいです。

--- share/app/lib/Dwarf/Validator/Constraint/Default.pm
+++ share/app/lib/Dwarf/Validator/Constraint/Default.pm
@@ -232,7 +232,7 @@ file_rule FILE_EXT => sub {
 	Carp::croak('missing args. usage: ["FILE_MIME", "text/plain"]') unless @_;
 	my $expected = $_[0];
 	my $ext = '';
-	if ($_->filename =~ /\.(.+)$/) {
+	if ($_->filename =~ /\.([^\.]+)$/) {
 		$ext = lc $1;
 	}
 	return $ext =~ /^$expected$/;
