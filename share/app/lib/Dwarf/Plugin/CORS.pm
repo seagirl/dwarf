3865991bcf725ec0f7d0b94d5112bf25b37b2077
Takuho Yoshizu <yoshizu@s2factory.co.jp>
2016-08-10 19:58:08 +0900

    CORS プラグインの修正

--- share/app/lib/Dwarf/Plugin/CORS.pm
+++ share/app/lib/Dwarf/Plugin/CORS.pm
@@ -8,6 +8,7 @@ sub init {
 	die "conf must be HASH" unless ref $conf eq 'HASH';
 
 	$conf->{origin}      ||= '*';
+	$conf->{methods}     ||= [qw/GET PUT POST DELETE HEAD OPTIONS/];
 	$conf->{headers}     ||= [qw/X-Requested-With/];
 	$conf->{credentials} ||= 0;
 	$conf->{maxage}      ||= 7200;
@@ -16,6 +17,7 @@ sub init {
 		my ($self, $res) = @_;
 		
 		$c->header('Access-Control-Allow-Origin' => $conf->{origin});
+		$c->header('Access-Control-Allow-Methods' => join ',', @{ $conf->{methods} });
 		$c->header('Access-Control-Allow-Headers' => join ',', @{ $conf->{headers} });
 
 		if ($conf->{credentials}) {
@@ -23,6 +25,9 @@ sub init {
 		}
 
 		if ($c->method eq 'OPTIONS' and $conf->{maxage}) {
+			# preflight なリクエストには 200 を返してしまう
+			$c->response->status(200);
+			$c->response->body("");
 			$c->header('Access-Control-Max-Age' => $conf->{maxage});
 		}
 	});
