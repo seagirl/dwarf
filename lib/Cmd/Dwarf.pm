0cfc2a1aac471691fa896da2e65dd6d53117992c
Takuho Yoshizu <yoshizu@s2factory.co.jp>
2016-09-26 23:54:48 +0900

    v1.41 RC1

--- lib/Cmd/Dwarf.pm
+++ lib/Cmd/Dwarf.pm
@@ -1,5 +1,5 @@
 package Cmd::Dwarf;
-our $VERSION = '1.31';
+our $VERSION = '1.41';
 1;
 __END__
 
--- share/app/lib/Dwarf.pm
+++ share/app/lib/Dwarf.pm
@@ -14,7 +14,7 @@ use Module::Find;
 use Router::Simple;
 use Scalar::Util qw/weaken/;
 
-our $VERSION = '1.31';
+our $VERSION = '1.41';
 
 use constant {
 	BEFORE_DISPATCH    => 'before_dispatch',
