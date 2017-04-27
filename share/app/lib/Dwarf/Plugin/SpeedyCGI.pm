195135a5aab4f101e7b8a53186448cfb08a489be
Jun Kuriyama <kuriyama@s2factory.co.jp>
2017-04-27 14:40:33 +0900

    CGI::SpeedyCGI 使ってるときに END ブロックが初回しか呼ばれない問題に対処

---
+++ share/app/lib/Dwarf/Plugin/SpeedyCGI.pm
@@ -0,0 +1,32 @@
+# Copyright (c) 2017  S2 Factory, Inc.  All rights reserved.
+#
+# CGI::SpeedyCGI 環境では、Perl の END ブロックは初回実行時のみにしか
+# 呼び出されないので、END ブロックに依存しているモジュールは個別に対処
+# する必要がある。
+#
+# 今のところ File::Temp の cleanup() 処理だけ。
+
+package Dwarf::Plugin::SpeedyCGI;
+use Dwarf::Pragma;
+use Dwarf::Util qw/installed/;
+
+sub init {
+	my ($class, $c, @args) = @_;
+	return if not installed("CGI::SpeedyCGI");
+
+	my $sp = CGI::SpeedyCGI->new;
+	return if not $sp->i_am_speedy;
+
+	$sp->register_cleanup(\&cleanup);
+}
+
+sub cleanup {
+	my ($self, $c) = @_;
+	# すでに use されている場合のみ明示的に cleanup() を呼び出す。
+	if (0 < grep { m|File/Temp\.pm| } keys %INC) {
+		# warn "Call File::Temp::cleanup() instead of END block.";
+		File::Temp::cleanup();
+	}
+}
+
+1;
