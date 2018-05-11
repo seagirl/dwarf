75553318109119316ab806804c7f3509119f78e2
Takuho Yoshizu <yoshizu@s2factory.co.jp>
2018-05-11 17:28:12 +0900

    適当にテスト書いた

--- share/app/lib/Dwarf/Plugin/Sentry.pm
+++ share/app/lib/Dwarf/Plugin/Sentry.pm
@@ -12,10 +12,10 @@ sub init {
 	my ($class, $c, $conf) = @_;
 	$conf ||= {};
 
-	die "dsn must be specified." unless $conf->{dsn};
-
 	add_method($c, call_sentry => sub {
 		my ($self, $message) = @_;
+		die "dsn must be specified." unless $conf->{dsn};
+		
 		chomp($message);
 
 		my $stacktrace = Devel::StackTrace->new(skip_frames => 1);
@@ -24,11 +24,7 @@ sub init {
 		my %stacktrace_context = $sentry->stacktrace_context($sentry->_get_frames_from_devel_stacktrace($stacktrace));
 
 		my $req = $self->request;
-		my $hdr = $req->headers->header_field_names->map(sub {
-			my $k = $_;
-			return { $k => $req->headers->header($_) };
-		});
-
+		my $hdr = [ map { my $k = $_; map { { $k => $_ } } $req->headers->header($_) } $req->headers->header_field_names ];
 		my %env = %{ $req->env };
 		foreach (keys %env) {
 			delete $env{$_} if (not /^HTTP_/);
